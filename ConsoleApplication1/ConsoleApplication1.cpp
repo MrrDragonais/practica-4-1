b68d46ac36a8319e0e3b752b1c2b80ed80d81fc5
nikit <nikit@DESKTOP-5363RTP>
2023-05-02 17:29:19 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+class Pen {
+public:
+	std::string color;
+};
+int main() {
+	using namespace std;
+	cout << "Test v1.01:OK.\n" << endl;
+	Pen pen;
+	pen.color = "#FFFFFF";
+	cout << "pen.color = " << pen.color << endl;
+	return 0;
+}
